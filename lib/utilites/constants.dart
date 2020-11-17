50e3c30fffa7c910dad6fdad1aeb39c0fb723341
Rahul Bera <bera2591@gmail.com>
2020-11-17 18:11:25 +0530

    constants.dart: Add constant colors and options

---
+++ lib/utilites/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color kScaffoldBackground = Color(0xFF011627);
+Color kActiveRadioButtonColor = Color(0xFFF95F62);
+enum kSearchOptions { set_number, train_number }
