d12d09b24156915384dd5644e8cdabf6f2cc67f7
Rahul Bera <bera2591@gmail.com>
2020-11-17 21:34:30 +0530

    constants.dart: Add kVerticalBar

--- lib/utilites/constants.dart
+++ lib/utilites/constants.dart
@@ -3,3 +3,14 @@ import 'package:flutter/material.dart';
 Color kScaffoldBackground = Color(0xFF011627);
 Color kActiveRadioButtonColor = Color(0xFFF95F62);
 enum kSearchOptions { set_number, train_number }
+//color: Color(0xFF505FE1),
+Widget kVerticalBar = Padding(
+  padding: EdgeInsets.symmetric(horizontal: 3.0),
+  child: Container(
+    color: Color(0xFF505FE1),
+    child: SizedBox(
+      width: 2.0,
+      height: 50.0,
+    ),
+  ),
+);
