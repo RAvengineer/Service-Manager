a9e2505a82990579994d184d1f6b91d5f853b38d
Rahul Bera <bera2591@gmail.com>
2020-11-17 18:12:20 +0530

    search_result.dart: Add ListView and Radio Buttons for search options

--- lib/screens/search_result.dart
+++ lib/screens/search_result.dart
@@ -1,23 +1,83 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:service_manager/utilites/constants.dart';
 import 'package:service_manager/widgets/common_appbar.dart';
 
-class SearchResult extends StatelessWidget {
+class SearchResult extends StatefulWidget {
   static const String id = "search_result";
+
+  @override
+  _SearchResultState createState() => _SearchResultState();
+}
+
+class _SearchResultState extends State<SearchResult> {
+  kSearchOptions _selectedOption = kSearchOptions.set_number;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-//      appBar: commonAppBar(context),
+      backgroundColor: kScaffoldBackground,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       body: SafeArea(
         child: Column(
           children: <Widget>[
             CommonAppBar(),
-            Container(
-              color: Colors.red,
-              child: SizedBox.fromSize(
-                size: Size.square(100.0),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 10.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(75.0),
+                    topLeft: Radius.circular(75.0),
+                  ),
+                ),
+                child: ListView(
+                  padding: EdgeInsets.only(top: 25.0),
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Radio(
+                          activeColor: kActiveRadioButtonColor,
+                          value: kSearchOptions.set_number,
+                          groupValue: _selectedOption,
+                          onChanged: (kSearchOptions value) {
+                            setState(() {
+                              _selectedOption = value;
+                            });
+                          },
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: const Text('Search Set Number'),
+                        ),
+                        Radio(
+                          activeColor: kActiveRadioButtonColor,
+                          value: kSearchOptions.train_number,
+                          groupValue: _selectedOption,
+                          onChanged: (kSearchOptions value) {
+                            setState(() {
+                              _selectedOption = value;
+                            });
+                          },
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: const Text('Search Set by Train Number'),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      color: Colors.red,
+                      child: SizedBox.fromSize(
+                        size: Size.square(100.0),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
+            CommonAppBar(),
           ],
         ),
       ),
