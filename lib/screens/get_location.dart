306febe866ffcf409405ee9e13e59b00e055f417
Rahul Bera <bera2591@gmail.com>
2020-11-18 23:49:13 +0530

    get_location.dart: Use GetAddress class to display the GPS coordinates on screen

--- lib/screens/get_location.dart
+++ lib/screens/get_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:service_manager/utilites/get_address.dart';
 
 class GetLocation extends StatefulWidget {
   static const id = "get_location";
@@ -8,6 +9,9 @@ class GetLocation extends StatefulWidget {
 }
 
 class _GetLocationState extends State<GetLocation> {
+  GetAddress _getAddress = GetAddress();
+  String gpsCoordinates = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,9 +24,17 @@ class _GetLocationState extends State<GetLocation> {
               MaterialButton(
                 color: Colors.lightGreenAccent,
                 child: Text("Get Location and Address"),
-                onPressed: () => {},
+                onPressed: () async {
+                  await _getAddress.checkLocationServices();
+                  if (!_getAddress.serviceEnabled) Navigator.maybePop(context);
+                  await _getAddress.getCurrentLocation();
+                  setState(() {
+                    gpsCoordinates =
+                        'GPS Coordinates: ${_getAddress.longitude} N, ${_getAddress.latitude} E ';
+                  });
+                },
               ),
-              Text("Location Obtained"),
+              Text('$gpsCoordinates\nAddress: '),
             ],
           ),
         ),
